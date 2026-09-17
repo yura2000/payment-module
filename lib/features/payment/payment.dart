39cc39ddeb45e3eaeda56678080c4b3cebad1ff1
Yura Uhryn <ugryn513@gmail.com>
2026-09-17 11:58:01 +0300

    feat(payment): add ResultView

--- lib/features/payment/payment.dart
+++ lib/features/payment/payment.dart
@@ -10,3 +10,4 @@ export 'src/presentation/can_pay.dart';
 export 'src/presentation/format_money.dart';
 export 'src/presentation/payment_brand_config.dart';
 export 'src/presentation/section_widgets.dart';
+export 'src/presentation/result_view.dart';
